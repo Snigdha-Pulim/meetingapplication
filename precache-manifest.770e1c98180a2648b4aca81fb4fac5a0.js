eeef26947ae681a8ad505053b6b8c974e01117aa
Snigdha Reddy Pulim <snigdhapulim2@gmail.com>
2019-10-25 14:25:06 +0530

    Updates

---
+++ precache-manifest.770e1c98180a2648b4aca81fb4fac5a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48c2d4a7a8c4283aec2cde80b8c4fd1c",
+    "url": "/meetingapplication/index.html"
+  },
+  {
+    "revision": "08116f5c6e9c08a22a18",
+    "url": "/meetingapplication/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "08116f5c6e9c08a22a18",
+    "url": "/meetingapplication/static/js/2.551c0468.chunk.js"
+  },
+  {
+    "revision": "c8c86376a4326d675cd0",
+    "url": "/meetingapplication/static/js/main.c47fdffa.chunk.js"
+  },
+  {
+    "revision": "91a80c3a1d0fc3173561",
+    "url": "/meetingapplication/static/js/runtime-main.3e9c86ae.js"
+  }
+]);
